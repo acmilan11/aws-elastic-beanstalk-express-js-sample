0e92c7ee6e18b0c3daf5ca1b63ec11d0632d2673
Maga <milan.maga@dxc.com>
2021-04-09 10:00:48 +0200

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Picus panelakovy'));
+app.get('/', (req, res) => res.send('zedol si mi capicu'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
